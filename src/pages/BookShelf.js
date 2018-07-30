809bf2cf22c1beba665c0c8d7d8c174d7b42c5d9
David Gaspar <davidgaspar.dev@gmail.com>
2018-07-30 02:56:35 -0300

    Added style to BookShelf

--- src/pages/BookShelf.js
+++ src/pages/BookShelf.js
@@ -6,15 +6,21 @@ import './BookShelf.css';
 
 export default class BookShelf extends Component {
 
+  state = {
+    myBooks: []
+  }
 
 
   render() {
-    const { Header } = this;
+    const { Header, Main } = this;
+    const { books } = this.state;
 
     return(
-      <div className='container' >
+      <div className='body-page' >
 
         <Header>My Reads</Header>
+
+        <Main books={books}/>
 
         <Link className='search-button' to='/search'>Search Book</Link>
 
@@ -22,12 +28,51 @@ export default class BookShelf extends Component {
     );
   }
 
+  /**
+   * Functinal Stateless Components
+   */
   Header = ({ children }) => (
     <header className='header'>
 
       <h1 className='header-title'>{children}</h1>
 
     </header>
+  );
+
+  Main = ({ books }) => (
+    <main className='main'>
+
+      <article>
+
+        <h2><strong>C</strong>urrently <strong>R</strong>eading</h2>
+
+        { /** code HERE */ }
+
+      </article>
+
+      <article>
+
+        <h2><strong>W</strong>ant to <strong>R</strong>ead</h2>
+
+        { /** code HERE */ }
+
+      </article>
+
+      <article>
+
+        <h2><strong>R</strong>ead</h2>
+
+        { /** code HERE */ }
+
+      </article>
+
+    </main>
+  );
+
+  Footer = () => (
+    <footer>
+
+    </footer>
   );
 
 }
