0bf09cd503b7bf8803d5e9d6d9d078ffbafe9e93
David Gaspar <davidgaspar.dev@gmail.com>
2018-07-31 00:24:01 -0300

    Get all books (BooksAPI)

--- src/pages/BookSearch.js
+++ src/pages/BookSearch.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+import * as BooksAPI from './utils/BooksAPI';
+
 import './BookSearch.css'
 
 export default class BookSearch extends Component {
 
   state = {
-    query: ''
+    query: '',
+    books: [],
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => this.setState({ books }));
   }
 
   render() {
-    const { SearchBar } = this;
-    const { query } = this.state;
+    const { SearchBar, Main } = this;
+    const { query, books } = this.state;
+
+    console.log(this.state);
 
     return(
       <div className='container' >
 
         <SearchBar query={query} />
+
+        <Main books={books} />
 
       </div>
     );
@@ -35,6 +47,16 @@ export default class BookSearch extends Component {
       </div>
 
     </header>
+  );
+
+  Main = ({ books }) => (
+    <main>
+
+      {
+        books.map(item => <h6>{item.title}</h6>)
+      }
+
+    </main>
   );
 
 }
--- src/pages/BookShelf.js
+++ src/pages/BookShelf.js
@@ -10,7 +10,6 @@ export default class BookShelf extends Component {
     myBooks: []
   }
 
-
   render() {
     const { Header, Main } = this;
     const { books } = this.state;
